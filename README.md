f0d2aa4f324d9a8644b02efd52383cdff70c9735
root <root@ip-172-31-20-43.us-east-2.compute.internal>
2024-11-06 13:47:24 +0000

    Initial commit with sample code

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Project
+This is a sample project for setting up a Jenkins CI/CD pipeline.
+
---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+def main():
+    print("Hello, Jenkins Pipeline!")
+
+if __name__ == "__main__":
+    main()
+
